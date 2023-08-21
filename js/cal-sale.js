d1e366c8d990ea5441bf6afe7141d03b7fb0afde
shahinur69 <shahinurmubarak238@gmail.com>
2023-08-21 21:45:19 +0600

    first sommits

---
+++ js/cal-sale.js
@@ -0,0 +1,7 @@
+function calKitchenWork(){
+    const find = document.getElementById('input-field');
+    console.log(find);
+    const field = getInputValue('input-field');
+    console.log(field);
+    return field, find;
+}
